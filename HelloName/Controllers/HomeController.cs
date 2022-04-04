8ddb889be74d9feaab3f00d27a44a045bb31de7c
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2022-04-05 01:56:53 +0300

    Task done

---
+++ HelloName/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using HelloName.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloName.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult HelloUser(User user)
+        {
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
